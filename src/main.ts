3b0ad8ee787f82cd1e97b0da64eb46dd36104c63
NichZilli <nich.zilli@hotmail.com>
2024-06-10 17:26:17 -0300

    update dotenv

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
