15f613f833628f3ba82d10421a72c3ee5d638b37
NichZilli <nich.zilli@hotmail.com>
2024-06-12 16:33:07 -0300

    add get all

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,18 +12,25 @@ export class UserController {
 
     constructor(private readonly userService: UserService) {}
 
-    @ApiOperation({ summary: 'Get a User by ID' })
-    @ApiResponse({ status: 200, description: 'OK', type: User })
-    @Get(':idUser')
-    public async getUser(@Param('id') id: string): Promise<CreateUserDto> {
-        return this.userService.getUser(id);
-    }
-
     @ApiOperation({ summary: 'Create a User' })
     @ApiBody({ type: CreateUserDto, description: 'The body for create a User' })
     @ApiResponse({ status: 201, description: 'User Created', type: User })
     @Post()
     public async createUser(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto> {
         return this.userService.createUser(createUserDto);
+    }
+
+    @ApiOperation({ summary: 'Get all Users' })
+    @ApiResponse({ status: 200, description: 'OK', type: [User] })
+    @Get()
+    public async getUsers(): Promise<User[]> {
+        return this.userService.getUsers();
+    }
+
+    @ApiOperation({ summary: 'Get a User by ID' })
+    @ApiResponse({ status: 200, description: 'OK', type: User })
+    @Get(':idUser')
+    public async getUser(@Param('id') id: string): Promise<CreateUserDto> {
+        return this.userService.getUser(id);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,45 +5,49 @@ import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class UserService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async getUser(id: string): Promise<User | null> {
-      const user = await this.prisma.user.findFirst({
-        where: { id: id }
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = await this.prisma.user.create({
+        data: {
+          userName: createUserDto.userName,
+          email: createUserDto.email,
+          password: createUserDto.password,
+          fullName: createUserDto.fullName
+        },
       });
+      return {
+        id: user.id,
+        userName: user.userName,
+        email: user.email,
+        password: user.password,
+        fullName: user.fullName,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+      };
+  }
 
-      if (user) {
-        return {
-          id: user.id,
-          userName: user.userName,
-          email: user.email,
-          password: user.password,
-          fullName: user.fullName,
-          createdAt: user.createdAt,
-          updatedAt: user.updatedAt
-        };
-      }
+  async getUsers(): Promise<User[]> {
+    return await this.prisma.user.findMany();
+  }
 
-      return null;
+  async getUser(id: string): Promise<User | null> {
+    const user = await this.prisma.user.findFirst({
+      where: { id: id }
+    });
+
+    if (user) {
+      return {
+        id: user.id,
+        userName: user.userName,
+        email: user.email,
+        password: user.password,
+        fullName: user.fullName,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+      };
     }
 
-    async createUser(createUserDto: CreateUserDto): Promise<User> {
-        const user = await this.prisma.user.create({
-            data: {
-              userName: createUserDto.userName,
-              email: createUserDto.email,
-              password: createUserDto.password,
-              fullName: createUserDto.fullName
-            },
-          });
-          return {
-            id: user.id,
-            userName: user.userName,
-            email: user.email,
-            password: user.password,
-            fullName: user.fullName,
-            createdAt: user.createdAt,
-            updatedAt: user.updatedAt
-          };
-    }
+    return null;
+  }
 }
